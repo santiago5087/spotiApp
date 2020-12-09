8ecb7c5f9e0b815ebe20931d437e44a8cbc6c17b
Santiago Marín <esmarina@unal.edu.co>
2020-12-08 22:47:39 -0500

    Get token automatically from Spotify

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
@@ -6,23 +6,28 @@ import { SpotifyService } from 'src/app/services/spotify.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
   newReleases: any[] = [];
   loading: boolean;
+  error: boolean;
+  errorMessage: string;
 
   constructor(private spotify: SpotifyService) {
     this.loading = true;
-
-    this.spotify.getNewReleases()
-      .subscribe((data) => {
-        console.log(data);
-        this.newReleases = data;
-        this.loading = false;
-      })
-  }
-
-  ngOnInit(): void {
+    this.error = false;
+    this.spotify.getTokenAuth().subscribe(data => {
+      this.spotify.getNewReleases()
+        .subscribe((data) => {
+          console.log(data);
+          this.newReleases = data;
+          this.loading = false;
+        }, (error) => {
+          this.error = true;
+          this.errorMessage = error['error']['error']['message'];
+          console.error(error);
+        });
+      });
   }
 
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable({
@@ -8,13 +8,17 @@ import { map } from 'rxjs/operators';
 })
 export class SpotifyService {
 
+  CLIENTID = "44e144db91b849f4b5b367e33a098006";
+  CLIENTSECRET = "cbcf411a84c442f5ba81068eb9a5bdeb";
+  token: string = undefined;
+
   constructor(private http: HttpClient) { }
 
   getQuery(query: string) {
     const url = `https://api.spotify.com/v1/${query}`;
 
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQCH4VJgQRI6Wsk_tulowUFfqLFGgJn-SIRP8A7A4NMPYXoSC92Ofc5-r58Poet-W4a8DPgx6DpXMpE0ilE'
+      'Authorization': 'Bearer ' + this.token
     });
 
     return this.http.get(url, { headers });
@@ -37,6 +41,21 @@ export class SpotifyService {
   getTopTracks(id: string): Observable<any> {
     return this.getQuery(`artists/${id}/top-tracks?country=US`)
       .pipe(map(data => data['tracks']));
+  }
+
+  getTokenAuth(): Observable<any> {
+    const body = new HttpParams()
+      .set('grant_type', 'client_credentials')
+      .set('client_id', this.CLIENTID)
+      .set('client_secret', this.CLIENTSECRET);
+    
+    return this.http.post('https://accounts.spotify.com/api/token', body.toString(),
+    { headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded') })
+      .pipe(map(res => {
+        localStorage.setItem('token', res['access_token']);
+        this.token = res['access_token'];
+        return this.token;
+      }));
   }
 
 }
